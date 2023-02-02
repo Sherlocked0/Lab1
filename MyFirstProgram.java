146fc0c0b398b133afc20439a2cc30448cbdd1bc
Georges Farah <georgesfarah403@gmail.com>
2023-02-02 14:22:20 +0200

    My First Program is done

---
+++ MyFirstProgram.java
@@ -0,0 +1,6 @@
+public class MyFirstProgram
+{
+public static void main (String [] args)
+{System.out.print("this is my");
+System.out.println("First Java Program");
+System.out.println("Programming is fun!");}}
